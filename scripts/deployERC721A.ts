383a1c6fa35406855bbfccdd08b6f2e33d4b4976
Don <don@don.dev>
2023-06-15 17:26:04 -0700

    fix: deploy script

--- scripts/deployERC721A.ts
+++ scripts/deployERC721A.ts
@@ -1,9 +1,13 @@
 const hre = require('hardhat');
+const { ethers } = require('hardhat');
 
 async function main() {
+    const [owner, addr1, addr2] = await ethers.getSigners();
     const factory = await hre.ethers.getContractFactory('ExampleERC721A');
-    const contract = await factory.deploy();
+    const contract = await factory.deploy(owner.address);
     await contract.deployed();
+
+    await contract.connect(owner).toggleIsPublicMintOpen();
 
     console.log(`✅ Contract deployed to address: ${contract.address}`);
 }
